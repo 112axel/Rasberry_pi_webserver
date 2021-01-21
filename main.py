6608f934e2883ae1e5ffd5730dd38e6545df7bcd
Axel Cullbrand <axel.cullbrand@gmail.com>
2021-01-21 13:34:40 +0100

    Added chat functionality to the webserver using flask socket io

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO,send
+import psutil
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "test" #better password needed
+socketio = SocketIO(app,cors_allowed_origins = "*")
+
+
+
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Hello worlds'
+
+"""
+@app.route("/recipe")
+def recipe_page():
+    return render_template("recipes.html",users = test)
+"""
+
+
+@app.route("/status")
+def status():
+    
+    return render_template("status.html",temp = psutil.disk_usage("C:")[0])
+
+@app.route("/chat")
+def chat_page():
+
+    return render_template("chat.html")
+
+
+@socketio.on("message")
+def handle_msg(msg):
+    print("Message: " + msg)
+    send(msg,broadcast = True)
 
     
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    socketio.run(app,host = "0.0.0.0",debug = False,port = 5000)
+
+    #app.run(debug=False, port=80, host='0.0.0.0')
